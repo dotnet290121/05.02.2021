e07fcb9a2db3257096031f72e3aa72b9b006f0fe
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-02-05 12:21:28 +0200

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -9,6 +9,28 @@ language plpgsql
         return query
         SELECT * FROM movies WHERE movies.price > _min_price;
     end;
-    $$
+    $$;
 
-select * from sp_get_movies_over(80)
+select * from sp_get_movies_over(80);
+
+create or replace FUNCTION sp_get_movies_data(discount boolean)
+returns TABLE(id bigint, title text, release_date timestamp, country_id bigint, price double precision)
+language plpgsql
+    AS
+    $$
+    BEGIN
+        -- update
+        -- inert
+        return query
+        SELECT movies.id, movies.title, movies.release_date, movies.country_id,
+               CASE WHEN discount = True THEN movies.price / 2.0 ELSE movies.price END
+               FROM movies ;
+    end;
+    $$;
+
+select * from sp_get_movies_data(TRUE);
+select * from sp_get_movies_data(FALSE);
+
+
+-- 1 create sp_get_country_starts_with (text) --> return table with all countries starting with the parameter
+-- 2 create sp_get_movies_short_long (bool) --> return table of movies join country if bool = true -> timestamp false release_date::date 
